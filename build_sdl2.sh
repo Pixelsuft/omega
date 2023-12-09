3f44f9884aed4c48c4f704c1ec1428752a30f4e7
Pixelsuft <yuskivl07@mail.ru>
2023-12-09 17:39:54 +0700

    wip

--- build_sdl2.sh
+++ build_sdl2.sh
@@ -3,4 +3,5 @@ if [ -z $CC ]; then
 fi
 CC=gcc
 SDL2_FLAGS=$(sdl2-config --cflags --libs)
-$CC src/*.c $SDL2_FLAGS $LIBS -Iinclude -DNOSTDLIB -DOMG_SDL2_DYNAMIC=0 -DOMG_DLL_BUILD -Wpedantic -Wall -Wextra -shared -fPIC -o libomega.so && $CC main.c -Wall -Wextra -Wpedantic -Iinclude -DOMG_SDL2_DYNAMIC=0 -DNOSTDLIB $SDL2_FLAGS $LIBS -lomega -L. -o omgapp
+LIBS="-lSDL2main -fno-stack-protector"
+$CC src/*.c $SDL2_FLAGS $LIBS -Iinclude -nostdlib -DNOSTDLIB -DOMG_SDL2_DYNAMIC=0 -DOMG_DLL_BUILD -Wpedantic -Wall -Wextra -shared -fPIC -o libomega.so && $CC main.c -Wall -Wextra -Wpedantic -Iinclude -nostdlib -DOMG_SDL2_DYNAMIC=0 -DNOSTDLIB $SDL2_FLAGS $LIBS -lomega -L. -o omgapp
