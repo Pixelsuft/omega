6be35c6a94cee6c93ea69687a13a9c9f8c8c3220
Pixelsuft <yuskivl07@mail.ru>
2023-12-17 09:24:56 +0700

    wip

--- src/window_win.c
+++ src/window_win.c
@@ -34,7 +34,7 @@ bool omg_window_win_init(OMG_WindowWin* this) {
         DWORD res;
         wchar_t* w_error_buffer;
         _OMG_WIN_FORMAT_ERROR(omg_base->mem, this->k32, error, w_error_buffer, res);
-        if ((omg_base->type == OMG_OMEGA_TYPE_WIN) && (res > 2)) {
+        if (res > 2) {
             w_error_buffer[res - 3] = L'\0';
             _OMG_LOG_ERROR(omg_base, "Failed to register class atom (", w_error_buffer, ")");
         }
@@ -42,6 +42,30 @@ bool omg_window_win_init(OMG_WindowWin* this) {
             _OMG_LOG_ERROR(omg_base, "Failed to register class atom (Error Code - ", error, ")");
         if (OMG_ISNOTNULL(w_error_buffer))
             OMG_FREE(omg_base->mem, w_error_buffer);
+        return true;
+    }
+    this->hwnd = this->u32->CreateWindowExW(
+        OMG_WIN_WS_EX_COMPOSITED | OMG_WIN_WS_EX_LAYERED | OMG_WIN_WS_EX_NOINHERITLAYOUT,
+        this->wc.lpszClassName, L"OMG Window",
+        WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        (int)base->size.w, (int)base->size.h,
+        NULL, NULL, this->wc.hInstance, NULL
+    );
+    if (OMG_ISNULL(this->hwnd)) {
+        DWORD error = this->k32->GetLastError();
+        DWORD res;
+        wchar_t* w_error_buffer;
+        _OMG_WIN_FORMAT_ERROR(omg_base->mem, this->k32, error, w_error_buffer, res);
+        if (res > 2) {
+            w_error_buffer[res - 3] = L'\0';
+            _OMG_LOG_ERROR(omg_base, "Failed to create window (", w_error_buffer, ")");
+        }
+        else
+            _OMG_LOG_ERROR(omg_base, "Failed to create window (Error Code - ", error, ")");
+        if (OMG_ISNOTNULL(w_error_buffer))
+            OMG_FREE(omg_base->mem, w_error_buffer);
+        this->u32->UnregisterClassW(this->wc.lpszClassName, this->wc.hInstance);
         return true;
     }
     base->inited = true;
