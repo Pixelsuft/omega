4e9d081aeaa8eb3a72970fae4053adc7d730f722
Pixelsuft <yuskivl07@mail.ru>
2023-12-29 12:48:56 +0700

    wip

--- include/omega/clock.h
+++ include/omega/clock.h
@@ -3,11 +3,14 @@
 
 typedef struct OMG_Clock {
     bool (*destroy)(struct OMG_Clock* this);
+    bool (*reset)(struct OMG_Clock* this);
+    bool (*update)(struct OMG_Clock* this);
     bool (*set_fps_limit)(struct OMG_Clock* this, double fps_limit);
     int (*get_fps)(struct OMG_Clock* this);
     void* omg;
     double dt;
     double fps_limit;
+    bool wait_for_limit;
     bool was_allocated;
 } OMG_Clock;
 
@@ -15,5 +18,7 @@ OMG_API bool omg_clock_init(OMG_Clock* this, bool high_perf);
 #if OMG_EXPORT_SHIT
 OMG_API int omg_clock_get_fps(OMG_Clock* this);
 OMG_API bool omg_clock_set_fps_limit(OMG_Clock* this, double fps_limit);
+OMG_API bool omg_clock_reset(OMG_Clock* this);
+OMG_API bool omg_clock_update(OMG_Clock* this);
 OMG_API bool omg_clock_destroy(OMG_Clock* this);
 #endif
--- src/clock.c
+++ src/clock.c
@@ -22,11 +22,24 @@ bool omg_clock_set_fps_limit(OMG_Clock* this, double fps_limit) {
     return false;
 }
 
+bool omg_clock_reset(OMG_Clock* this) {
+    OMG_UNUSED(this);
+    return false;
+}
+
+bool omg_clock_update(OMG_Clock* this) {
+    OMG_UNUSED(this);
+    return false;
+}
+
 bool omg_clock_init(OMG_Clock* this, bool high_perf) {
     OMG_UNUSED(high_perf);
+    this->wait_for_limit = true;
     this->dt = 0.0;
     this->fps_limit = 0.0;
     this->destroy = omg_clock_destroy;
+    this->reset = omg_clock_reset;
+    this->update = omg_clock_update;
     this->get_fps = omg_clock_get_fps;
     this->set_fps_limit = omg_clock_set_fps_limit;
     return false;
