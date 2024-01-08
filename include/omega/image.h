4b148b01956f797465f309ecb749647403ee3200
Pixelsuft <yuskivl07@mail.ru>
2024-01-08 15:05:13 +0700

    wip

--- include/omega/image.h
+++ include/omega/image.h
@@ -7,6 +7,7 @@
 typedef struct OMG_ImageLoader {
     int type;
     bool (*init)(struct OMG_ImageLoader* this);
+    void* (*image_from_fp_internal)(struct OMG_ImageLoader* this, const OMG_String* path);
     bool (*destroy)(struct OMG_ImageLoader* this);
     bool inited;
     bool was_allocated;
@@ -14,3 +15,6 @@ typedef struct OMG_ImageLoader {
 
 OMG_API bool omg_image_loader_init(OMG_ImageLoader* this);
 OMG_API bool omg_image_loader_destroy(OMG_ImageLoader* this);
+#if OMG_EXPORT_SHIT
+OMG_API void* omg_image_loader_image_from_fp(OMG_ImageLoader* this, const OMG_String* path);
+#endif
--- src/image.c
+++ src/image.c
@@ -5,9 +5,15 @@ bool omg_image_loader_destroy(OMG_ImageLoader* this) {
     return false;
 }
 
+void* omg_image_loader_image_from_fp(OMG_ImageLoader* this, const OMG_String* path) {
+    OMG_UNUSED(this, path);
+    return NULL;
+}
+
 bool omg_image_loader_init(OMG_ImageLoader* this) {
     this->inited = true;
     this->type = OMG_IMAGE_LOADER_TYPE_NONE;
     this->destroy = omg_image_loader_destroy;
+    this->image_from_fp_internal = omg_image_loader_image_from_fp;
     return false;
 }
