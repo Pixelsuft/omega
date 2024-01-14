9eef20c59cfc860dc04d807ba4d76973edd0fd5f
Pixelsuft <yuskivl07@mail.ru>
2024-01-14 19:35:13 +0700

    wip

--- build_mingw_win32.bat
+++ build_mingw_win32.bat
@@ -1,4 +1,4 @@
 @echo off
-set EXTRA=-lkernel32 -lgcc -lntdll -lm -DOMG_SUPPORT_RAYLIB=0 -lmingw32
+set EXTRA=-lkernel32 -lgcc -lntdll -lm -DOMG_SUPPORT_RAYLIB=0 -lmingw32 -nostdlib -DNOSTDLIB
 rem gcc main.c src/*.c -municode -DOMG_STATIC -DNOSTDLIB %LIBS% -Wall -Wextra -Wpedantic -Iinclude -nostdlib -mwindows %* -o omgapp.exe
 gcc src/*.c -Iinclude -municode -DOMG_DLL_BUILD -Wpedantic -Wall -Wextra %EXTRA% -shared -fPIC -o omega.dll -mwindows && gcc main.c -Wall -Wextra -Wpedantic -Iinclude -lomega -mwindows -municode -L. -o omgapp.exe %EXTRA%
