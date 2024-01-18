203767dc9088add3bed1b4ad13b2a91dfc536002
Pixelsuft <yuskivl07@mail.ru>
2024-01-18 18:49:52 +0700

    todo

--- main.c
+++ main.c
@@ -308,9 +308,10 @@ void app_init(App* this, OMG_EntryData* data) {
     this->win->set_min_size(this->win, &OMG_FPOINT_MAKE(320, 200));
     this->omg->winmgr->surf_rle = false; // Don't need to lock surface
     this->surf = this->omg->winmgr->surf_create(this->omg->winmgr, &OMG_FPOINT_MAKE(256, 256), true);
-    for (uint64_t* i = this->surf->data; (size_t)i - (size_t)this->surf->data < (size_t)this->surf->size.w * (size_t)this->surf->size.h * 4; i++) {
-        *i = 0x000000FF | (uint64_t)((double)((size_t)i - (size_t)this->surf->data) * (double)0x4000);
-    }
+    if (OMG_ISNOTNULL(this->surf->data))
+        for (uint64_t* i = this->surf->data; (size_t)i - (size_t)this->surf->data < (size_t)this->surf->size.w * (size_t)this->surf->size.h * 4; i++) {
+            *i = 0x000000FF | (uint64_t)((double)((size_t)i - (size_t)this->surf->data) * (double)0x4000);
+        }
     this->win->set_icon(this->win, this->surf);
     this->tex = this->ren->tex_create(this->ren, &OMG_FPOINT_MAKE(200, 200), OMG_TEXTURE_ACCESS_TARGET, true);
     this->tex2 = OMG_REN_TEXTURE_FROM_FILE(this->ren, &OMG_STRING_MAKE_STATIC("assets/sprite.png"));
