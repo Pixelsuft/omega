ed3c6714886ac2de0db947ef066b40a8868493ea
Pixelsuft <yuskivl07@mail.ru>
2024-01-20 20:47:02 +0700

    wip

--- include/omega/api_sdl2_ttf.h
+++ include/omega/api_sdl2_ttf.h
@@ -16,11 +16,21 @@
 #define UNICODE_BOM_NATIVE  0xFEFF
 #define UNICODE_BOM_SWAPPED 0xFFFE
 
-#define TTF_STYLE_NORMAL        0x00
-#define TTF_STYLE_BOLD          0x01
-#define TTF_STYLE_ITALIC        0x02
-#define TTF_STYLE_UNDERLINE     0x04
+#define TTF_STYLE_NORMAL 0x00
+#define TTF_STYLE_BOLD 0x01
+#define TTF_STYLE_ITALIC 0x02
+#define TTF_STYLE_UNDERLINE 0x04
 #define TTF_STYLE_STRIKETHROUGH 0x08
+
+#define TTF_HINTING_NORMAL 0
+#define TTF_HINTING_LIGHT 1
+#define TTF_HINTING_MONO 2
+#define TTF_HINTING_NONE 3
+#define TTF_HINTING_LIGHT_SUBPIXEL 4
+
+#define TTF_WRAPPED_ALIGN_LEFT 0
+#define TTF_WRAPPED_ALIGN_CENTER 1
+#define TTF_WRAPPED_ALIGN_RIGHT 2
 
 typedef void TTF_Font;
 #else
@@ -43,6 +53,26 @@ typedef struct {
     int OMG_SDL2_TTF_STD_PREFIX (*TTF_SetFontSize)(TTF_Font*, int);
     int OMG_SDL2_TTF_STD_PREFIX (*TTF_SetFontSizeDPI)(TTF_Font*, int, unsigned int, unsigned int);
     int OMG_SDL2_TTF_STD_PREFIX (*TTF_GetFontStyle)(const TTF_Font*);
+    void OMG_SDL2_TTF_STD_PREFIX (*TTF_SetFontStyle)(TTF_Font*, int);
+    int OMG_SDL2_TTF_STD_PREFIX (*TTF_GetFontOutline)(const TTF_Font*);
+    void OMG_SDL2_TTF_STD_PREFIX (*TTF_SetFontOutline)(TTF_Font*, int);
+    int OMG_SDL2_TTF_STD_PREFIX (*TTF_GetFontHinting)(const TTF_Font*);
+    void OMG_SDL2_TTF_STD_PREFIX (*TTF_SetFontHinting)(TTF_Font*, int);
+    int OMG_SDL2_TTF_STD_PREFIX (*TTF_GetFontWrappedAlign)(const TTF_Font*);
+    void OMG_SDL2_TTF_STD_PREFIX (*TTF_SetFontWrappedAlign)(TTF_Font*, int);
+    int OMG_SDL2_TTF_STD_PREFIX (*TTF_FontHeight)(const TTF_Font*);
+    int OMG_SDL2_TTF_STD_PREFIX (*TTF_FontAscent)(const TTF_Font*);
+    int OMG_SDL2_TTF_STD_PREFIX (*TTF_FontDescent)(const TTF_Font*);
+    int OMG_SDL2_TTF_STD_PREFIX (*TTF_FontLineSkip)(const TTF_Font*);
+    int OMG_SDL2_TTF_STD_PREFIX (*TTF_GetFontKerning)(const TTF_Font*);
+    void OMG_SDL2_TTF_STD_PREFIX (*TTF_SetFontKerning)(TTF_Font*, int);
+    long OMG_SDL2_TTF_STD_PREFIX (*TTF_FontFaces)(const TTF_Font*);
+    int OMG_SDL2_TTF_STD_PREFIX (*TTF_FontFaceIsFixedWidth)(const TTF_Font*);
+    const char* OMG_SDL2_TTF_STD_PREFIX (*TTF_FontFaceFamilyName)(const TTF_Font*);
+    const char* OMG_SDL2_TTF_STD_PREFIX (*TTF_FontFaceStyleName)(const TTF_Font*);
+    int OMG_SDL2_TTF_STD_PREFIX (*TTF_GlyphIsProvided)(TTF_Font*, uint16_t ch);
+    int OMG_SDL2_TTF_STD_PREFIX (*TTF_GlyphIsProvided32)(TTF_Font*, uint32_t);
+    int OMG_SDL2_TTF_STD_PREFIX (*TTF_GlyphMetrics)(TTF_Font*, uint16_t, int*, int*, int*, int*, int*);
     SDL_version ver;
 } OMG_Sdl2Ttf;
 
