b4d5d4602b1ce595dae46364981008f9182e818e
Pixelsuft <yuskivl07@mail.ru>
2024-01-21 08:45:13 +0700

    openbsd fix

--- build_sdl2.sh
+++ build_sdl2.sh
@@ -2,5 +2,5 @@ if [ -z $CC ]; then
     CC=gcc
 fi
 SDL2_FLAGS=$(sdl2-config --cflags --libs)
-EXTRA="-lSDL2main -DOMG_SUPPORT_RAYLIB=0 -fno-stack-protector -Wno-gnu-zero-variadic-macro-arguments -Wno-empty-translation-unit"
+EXTRA="-lSDL2main -lm -DOMG_SUPPORT_RAYLIB=0 -fno-stack-protector -Wno-gnu-zero-variadic-macro-arguments -Wno-empty-translation-unit"
 $CC src/*.c $SDL2_FLAGS -Iinclude -DOMG_DLL_BUILD -Wpedantic -Wall -Wextra $EXTRA -shared -fPIC -o libomega.so && $CC main.c -Wall -Wextra -Wpedantic $EXTRA -Iinclude $SDL2_FLAGS -lomega -L. -o omgapp
