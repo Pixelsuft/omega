abdce931ce4e6c273efc1048c0761fe4497caca4
Pixelsuft <yuskivl07@mail.ru>
2024-03-02 10:41:12 +0700

    wip

--- include/omega/scene_objects.h
+++ include/omega/scene_objects.h
@@ -15,8 +15,20 @@ typedef struct {
     bool running;
 } OMG_ObjectTimer;
 
+typedef struct {
+    OMG_Object base;
+    OMG_Clock* clock;
+    double counter;
+    double duration;
+    bool soft;
+    bool running;
+    bool repeat;
+} OMG_ObjectAnimTimer;
+
 OMG_API bool omg_obj_timer_init(OMG_ObjectTimer* this, OMG_Omega* omg);
+OMG_API bool omg_obj_anim_timer_init(OMG_ObjectAnimTimer* this, OMG_Omega* omg);
 #if OMG_EXPORT_SHIT
 OMG_API bool omg_obj_timer_on_update(OMG_ObjectTimer* this, OMG_Scene* scene);
+OMG_API bool omg_obj_anim_timer_on_update(OMG_ObjectAnimTimer* this, OMG_Scene* scene);
 #endif
 #endif
--- src/scene_objects.c
+++ src/scene_objects.c
@@ -33,12 +33,49 @@ bool omg_obj_timer_init(OMG_ObjectTimer* this, OMG_Omega* omg) {
     this->clock = omg->clock;
     this->duration = 0.0;
     this->counter = 0.0;
-    this->triggered = false;
+    this->triggered = 0;
     this->repeat = true;
     this->running = false;
     this->soft = false;
     OMG_BEGIN_POINTER_CAST();
     obj_base->on_update = omg_obj_timer_on_update;
+    obj_base->on_paint = NULL;
+    OMG_END_POINTER_CAST();
+    return false;
+}
+
+bool omg_obj_anim_timer_on_update(OMG_ObjectAnimTimer* this, OMG_Scene* scene) {
+    if (!this->running)
+        return false;
+    this->counter += this->clock->dt;
+    if (this->soft) {
+        if (this->counter >= this->duration) {
+            this->counter -= this->duration;
+            if (!this->repeat)
+                this->running = false;
+        }
+    }
+    else {
+        while (this->counter >= this->duration) {
+            this->counter -= this->duration;
+            if (!this->repeat) {
+                this->running = false;
+                break;
+            }
+        }
+    }
+    return false;
+}
+
+bool omg_obj_anim_timer_init(OMG_ObjectAnimTimer* this, OMG_Omega* omg) {
+    this->clock = omg->clock;
+    this->duration = 0.0;
+    this->counter = 0.0;
+    this->repeat = true;
+    this->running = false;
+    this->soft = false;
+    OMG_BEGIN_POINTER_CAST();
+    obj_base->on_update = omg_obj_anim_timer_on_update;
     obj_base->on_paint = NULL;
     OMG_END_POINTER_CAST();
     return false;
