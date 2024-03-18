b414d9a4130e6218140840927e2623c3de37d8e3
Pixelsuft <yuskivl07@mail.ru>
2024-03-18 19:28:45 +0700

    wip

--- game/aamain.c
+++ game/aamain.c
@@ -2,11 +2,6 @@
 #include <omega/entry.h>
 #include <omega/api_win.h>
 #include <omega/api_static.h>
-#include <omega/scene.h>
-#include <omega/scene_objects.h>
-#include <omega/array.h>
-#include <omega/bmfont.h>
-#include <omega/ldtk.h>
 
 int omega_main(OMG_EntryData* data);
 OMG_MAIN_MAKE(omega_main)
